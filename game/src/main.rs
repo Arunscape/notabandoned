00d76aa9bc00db4a62390b75ce3d1970aa1a6ec2
Arunscape <arun@woosaree.xyz>
2020-10-05 17:49:18 -0600

    idk

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,9 +1,38 @@
 #![warn(clippy::all)]
 
 use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
+use winit::monitor::MonitorHandle;
+use winit::window::{Fullscreen, Window, WindowBuilder};
+
+#[derive(Debug)]
+pub struct WinitState {
+    pub events_loop: EventLoop<()>,
+    pub window: Window,
+}
+
+impl WinitState {
+    /// It's possible for the window creation to fail. This is unlikely.
+    pub fn new<T: Into<String>>(title: T) -> Result<Self, CreationError> {
+        let events_loop = EventsLoop::new();
+        let output = WindowBuilder::new()
+            .with_fullscreen(Some(Fullscreen::Borderless(None)))
+            .build(&events_loop);
+        output.map(|window| Self {
+            events_loop,
+            window,
+        })
+    }
+}
+
+impl Default for WinitState {
+    fn default() -> Self {
+        Self::new("Chungus").expect("Could not create a window!")
+    }
+}
 
 fn main() {
+    let mut winit_state = WinitState::default();
+
     let events_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("YEEEEEEEEEEEE")
---
+++ game_lib/src/lib.rs
@@ -0,0 +1,4 @@
+mod game;
+#[warn(clippy::all)]
+mod io;
+mod player;
