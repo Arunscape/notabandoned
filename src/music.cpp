93836e8346c4286073b83eb2930d0b3420ff7ede
Arunscape <arun@woosaree.xyz>
2020-10-10 14:22:34 -0600

    clang-format

--- src/music.cpp
+++ src/music.cpp
@@ -12,7 +12,7 @@ Asound::Asound() {
 
   file >> config;
 
-  for (auto &[k, v] : config.items()){
+  for (auto &[k, v] : config.items()) {
     std::cout << "Read sound key: " << k << " val: " << v << std::endl;
     std::string s = MUSIC_CONFIG_PATH + std::string(v);
     sounds.emplace(k, s);
@@ -21,73 +21,73 @@ Asound::Asound() {
 
 Asound::~Asound() {}
 
-raylib::Sound& Asound::play(const std::string sound){
+raylib::Sound &Asound::play(const std::string sound) {
   return (*this)[sound].Play();
 }
 
-raylib::Sound& Asound::pause(const std::string &sound) {
+raylib::Sound &Asound::pause(const std::string &sound) {
   return (*this)[sound].Pause();
 }
 
-raylib::Sound& Asound::resume(const std::string &sound) {
+raylib::Sound &Asound::resume(const std::string &sound) {
   return (*this)[sound].Resume();
 }
 
-raylib::Sound& Asound::stop(const std::string &sound){
+raylib::Sound &Asound::stop(const std::string &sound) {
   return (*this)[sound].Stop();
 }
 
-raylib::Sound& Asound::play_multi(const std::string &sound){
+raylib::Sound &Asound::play_multi(const std::string &sound) {
   return (*this)[sound].PlayMulti();
 }
 
-raylib::Sound& Asound::stop_multi(const std::string &sound){
+raylib::Sound &Asound::stop_multi(const std::string &sound) {
   return (*this)[sound].StopMulti();
 }
 
-raylib::Sound& Asound::set_pitch(const std::string &sound, float pitch){
+raylib::Sound &Asound::set_pitch(const std::string &sound, float pitch) {
   return (*this)[sound].SetPitch(pitch);
 }
 
-raylib::Sound& Asound::set_volume(const std::string &sound, float volume){
+raylib::Sound &Asound::set_volume(const std::string &sound, float volume) {
   return (*this)[sound].SetVolume(volume);
 }
 
-bool Asound::is_playing(const std::string &sound){
+bool Asound::is_playing(const std::string &sound) {
   return (*this)[sound].IsPlaying();
 }
 
-unsigned int Asound::get_sample_count(const std::string &sound){
+unsigned int Asound::get_sample_count(const std::string &sound) {
   return (*this)[sound].GetSampleCount();
 }
 
-void Asound::set_sample_count(const std::string &sound, unsigned int value){
+void Asound::set_sample_count(const std::string &sound, unsigned int value) {
   (*this)[sound].SetSampleCount(value);
 }
 
-raylib::AudioStream Asound::get_audio_stream(const std::string &sound){
+raylib::AudioStream Asound::get_audio_stream(const std::string &sound) {
   return (*this)[sound].GetStream();
 }
 
-void Asound::set_audio_stream(const std::string &sound, raylib::AudioStream value){
+void Asound::set_audio_stream(const std::string &sound,
+                              raylib::AudioStream value) {
   (*this)[sound].SetStream(value);
 }
 
-void Asound::unload(const std::string &sound){
-  (*this)[sound].Unload();
-}
+void Asound::unload(const std::string &sound) { (*this)[sound].Unload(); }
 
-raylib::Sound& Asound::update(const std::string &sound, const void* data, int sample_count){
+raylib::Sound &Asound::update(const std::string &sound, const void *data,
+                              int sample_count) {
   return (*this)[sound].Update(data, sample_count);
 }
 
-void Asound::set(const std::string &sound, raylib::Sound rsound){
+void Asound::set(const std::string &sound, raylib::Sound rsound) {
   (*this)[sound].set(rsound);
 }
 
-raylib::Sound& Asound::operator[](const std::string &sound) {
-    if (sounds.find(sound) == sounds.end())
-        throw std::runtime_error("sound not found");
+raylib::Sound &Asound::operator[](const std::string &sound) {
+  if (sounds.find(sound) == sounds.end())
+    throw std::runtime_error("sound not found");
 
-    return sounds.find(sound)->second;
+  return sounds.find(sound)->second;
 }
--- src/music.h
+++ src/music.h
@@ -13,14 +13,14 @@ class Asound {
 public:
   Asound();
   ~Asound();
-  raylib::Sound& play(std::string sound);
-  raylib::Sound& pause(const std::string &sound);
-  raylib::Sound& resume(const std::string &sound);
-  raylib::Sound& stop(const std::string &sound);
-  raylib::Sound& play_multi(const std::string &sound);
-  raylib::Sound& stop_multi(const std::string &sound);
-  raylib::Sound& set_pitch(const std::string &sound, float pitch);
-  raylib::Sound& set_volume(const std::string &sound, float volume);
+  raylib::Sound &play(std::string sound);
+  raylib::Sound &pause(const std::string &sound);
+  raylib::Sound &resume(const std::string &sound);
+  raylib::Sound &stop(const std::string &sound);
+  raylib::Sound &play_multi(const std::string &sound);
+  raylib::Sound &stop_multi(const std::string &sound);
+  raylib::Sound &set_pitch(const std::string &sound, float pitch);
+  raylib::Sound &set_volume(const std::string &sound, float volume);
   bool is_playing(const std::string &sound);
   unsigned int get_sample_count(const std::string &sound);
   void set_sample_count(const std::string &sound, unsigned int value);
@@ -28,6 +28,7 @@ public:
   void set_audio_stream(const std::string &sound, raylib::AudioStream value);
   void unload(const std::string &sound);
   void set(const std::string &sound, raylib::Sound rsound);
-  raylib::Sound& update(const std::string &sound, const void* data, int sample_count);
-  raylib::Sound& operator[](const std::string &sound);
+  raylib::Sound &update(const std::string &sound, const void *data,
+                        int sample_count);
+  raylib::Sound &operator[](const std::string &sound);
 };
